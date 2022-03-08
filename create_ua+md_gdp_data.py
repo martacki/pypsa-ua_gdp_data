8764b73cecba5ed5095a8437ffba5ccc68e5a61c
martacki <martha.frysztacki@kit.edu>
2022-03-08 12:10:09 +0100

    include population data

--- create_ua+md_gdp_data.py
+++ create_ua+md_gdp_data.py
@@ -1,32 +1,44 @@
+
+import numpy as np
 import xarray as xr
+import rioxarray as rxr
 import geopandas as gpd
 import xagg as xa
 import pandas as pd
 
-gdp = xr.open_dataset('data/GDP_PPP_30arcsec_v3.nc').sel(time=2015)
+gdp = xr.open_dataset('data/GDP_per_capita_PPP_1990_2015_v2.nc').sel(time=2015).GDP_per_capita_PPP
+pop = (rxr.open_rasterio('data/ppp_2013_1km_Aggregated.tif').sel(band=1)
+       .rename({'x': 'longitude', 'y': 'latitude'}))
 onshore_region = gpd.read_file('data/regions_onshore.geojson').query("country in ['UA', 'MD']")
-
 
 idx = onshore_region.index[0]
 x = onshore_region.loc[idx].x
 y = onshore_region.loc[idx].y
 
+# create cutouts
 region_cutout = onshore_region.loc[idx:idx]
 gdp_cutout = gdp.sel(longitude=slice(x-.5, x+.5), latitude=slice(y+.5, y-.5))
+pop_cutout = (pop.sel(longitude=slice(x-.5, x+.5), latitude=slice(y+.5, y-.5))
+              .where(lambda x: x>=0, np.nan))
 
-weightmap = xa.pixel_overlaps(gdp_cutout, region_cutout)
-aggregated = xa.aggregate(gdp_cutout, weightmap)
+# interpolate different resolutions
+gdp_cutout = gdp_cutout.interp_like(pop_cutout)
 
-gdp_ua_df = pd.DataFrame(
+# weighting for gdp and pop
+gdp_pop_cutout = (0.6 * gdp_cutout + 0.4) * pop_cutout
+
+weightmap = xa.pixel_overlaps(gdp_pop_cutout, region_cutout)
+aggregated = xa.aggregate(gdp_pop_cutout, weightmap)
+
+gdp_pop_df = pd.DataFrame(
     columns = aggregated.to_dataset().name,
-    index = ['GDP_PPP', 'country'],
-    data = [aggregated.to_dataset().GDP_PPP.values, aggregated.to_dataset().country.values]
+    index = ['pop_gdp', 'country'],
+    data = [aggregated.to_dataset()['var'].values, aggregated.to_dataset()['country'].values]
 ).T
 
-gdp_ua_df.to_csv('results/GDP_PPP_30arcsec_v3_mapped.csv')
+gdp_pop_df.to_csv('results/ppp_2013_1km_Aggregated_and_GDP_per_capita_PPP_1990_2015_v2_mapped.csv')
 
 # for some reason, passing all gdp+regions doesn't work. therefore, solve in a for-loop
-
 counter = 1
 print(f"{counter}/{len(onshore_region)}")
 print("--- --- ---")
@@ -34,20 +46,31 @@ for idx in onshore_region.index[1:]:
     x = onshore_region.loc[idx].x
     y = onshore_region.loc[idx].y
 
+    # create cutouts
     region_cutout = onshore_region.loc[idx:idx]
     gdp_cutout = gdp.sel(longitude=slice(x-.5, x+.5), latitude=slice(y+.5, y-.5))
+    pop_cutout = (pop.sel(longitude=slice(x-.5, x+.5), latitude=slice(y+.5, y-.5))
+                  .where(lambda x: x>=0, np.nan))
+
+    # interpolate different resolutions
+    gdp_cutout = gdp_cutout.interp_like(pop_cutout)
+
+    # combine datasets with different weighting for gdp and pop
+    gdp_pop_cutout = (0.6 * gdp_cutout + 0.4) * pop_cutout
     
-    weightmap = xa.pixel_overlaps(gdp_cutout, region_cutout)
-    aggregated = xa.aggregate(gdp_cutout, weightmap)
+    weightmap = xa.pixel_overlaps(gdp_pop_cutout, region_cutout)
+    aggregated = xa.aggregate(gdp_pop_cutout, weightmap)
     
-    gdp_ua_df_cutout = pd.DataFrame(
+    gdp_pop_cutout_df = pd.DataFrame(
         columns = aggregated.to_dataset().name,
-        index = ['GDP_PPP', 'country'],
-        data = [aggregated.to_dataset().GDP_PPP.values, aggregated.to_dataset().country.values]
+        index = ['pop_gdp', 'country'],
+        data = [aggregated.to_dataset()['var'].values, aggregated.to_dataset()['country'].values]
     ).T
     
-    gdp_ua_df = gdp_ua_df.append(gdp_ua_df_cutout)
-    gdp_ua_df.to_csv('results/GDP_PPP_30arcsec_v3_mapped.csv')
+    gdp_pop_df = gdp_pop_df.append(gdp_pop_cutout_df)
+    gdp_pop_df.to_csv(
+        'results/ppp_2013_1km_Aggregated_and_GDP_per_capita_PPP_1990_2015_v2_mapped.csv'
+    )
     
     counter+=1
     print(f"{counter}/{len(onshore_region)}")
